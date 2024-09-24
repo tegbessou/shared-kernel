d7acaf595631403bf63319004fa151f566431446
Tegbessou <hugues.gobet@gmail.com>
2024-09-24 20:31:54 +0200

    Do some clean

--- src/Infrastructure/DependencyInjection/TegCorpSharedKernelExtension.php
+++ src/Infrastructure/DependencyInjection/TegCorpSharedKernelExtension.php
@@ -15,21 +15,17 @@ class TegCorpSharedKernelExtension extends Extension implements PrependExtension
 {
     public function prepend(ContainerBuilder $container)
     {
-        // Vérifier si le FrameworkBundle est disponible
         if (!$container->hasExtension('framework')) {
-            die('hugues');
             throw new \LogicException('Le FrameworkBundle doit être installé et activé pour utiliser le SharedKernelBundle.');
         }
 
-        // Charger la configuration Messenger
-        $configs = Yaml::parseFile(__DIR__.'/../Symfony/Resources/config/packages/messenger.yaml');
+        $configs = Yaml::parseFile(__DIR__.'/../../Symfony/Resources/config/packages/messenger.yaml');
         $container->prependExtensionConfig('framework', $configs['framework']);
     }
 
     public function load(array $configs, ContainerBuilder $container)
     {
-        // Charger les services du bundle
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Symfony/Resources/config/packages'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../Symfony/Resources/config/packages'));
         $loader->load('messenger.yaml');
     }
 }
