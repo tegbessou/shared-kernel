83f7c027abde54e8a5b8663ec4aebe0881e7dde2
Tegbessou <hugues.gobet@gmail.com>
2024-09-24 21:28:39 +0200

    Do some clean

--- src/TegCorpSharedKernelBundle.php
+++ src/TegCorpSharedKernelBundle.php
@@ -9,12 +9,23 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\Yaml\Yaml;
 
 class TegCorpSharedKernelBundle extends AbstractBundle
 {
+    public function configureContainer(ContainerConfigurator $container): void
+    {
+        $container->import(__DIR__.'/../config/{services,messenger}.yaml');
+    }
+
+    /*public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $configs = Yaml::parseFile(__DIR__.'/../config/messenger.yaml');
+        $builder->prependExtensionConfig('framework', $configs['messenger']);
+    }
+
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        // load an XML, PHP or YAML file
         $loader = new YamlFileLoader($builder, new FileLocator(__DIR__.'/../config'));
         $loader->load('messenger.yaml');
         $loader->load('services.yaml');
@@ -23,5 +34,5 @@ class TegCorpSharedKernelBundle extends AbstractBundle
     public function getPath(): string
     {
         return \dirname(__DIR__);
-    }
+    }*/
 }
