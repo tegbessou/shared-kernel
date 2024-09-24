68e317e873760dec9bd2c489c1080a49dc782cbe
Tegbessou <hugues.gobet@gmail.com>
2024-09-24 20:11:57 +0200

    Do some clean

--- src/Infrastructure/DependencyInjection/TegCorpSharedKernelExtension.php
+++ src/Infrastructure/DependencyInjection/TegCorpSharedKernelExtension.php
@@ -7,14 +7,29 @@ namespace TegCorp\SharedKernelBundle\Infrastructure\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
 
-class TegCorpSharedKernelExtension extends Extension
+class TegCorpSharedKernelExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container)
+    {
+        // Vérifier si le FrameworkBundle est disponible
+        if (!$container->hasExtension('framework')) {
+            die('hugues');
+            throw new \LogicException('Le FrameworkBundle doit être installé et activé pour utiliser le SharedKernelBundle.');
+        }
+
+        // Charger la configuration Messenger
+        $configs = Yaml::parseFile(__DIR__.'/../Symfony/Resources/config/packages/messenger.yaml');
+        $container->prependExtensionConfig('framework', $configs['framework']);
+    }
+
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-
+        // Charger les services du bundle
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Symfony/Resources/config/packages'));
         $loader->load('messenger.yaml');
     }
 }
--- src/TegCorpSharedKernelBundle.php
+++ src/TegCorpSharedKernelBundle.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace TegCorp\SharedKernelBundle;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use TegCorp\SharedKernelBundle\Infrastructure\DependencyInjection\TegCorpSharedKernelExtension;
 
 class TegCorpSharedKernelBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (null === $this->extension) {
+            $this->extension = new TegCorpSharedKernelExtension();
+        }
+
+        return $this->extension;
+    }
 }
