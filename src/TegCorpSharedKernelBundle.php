ace55801510dad13692fd6da515e7039f2a9a597
Tegbessou <hugues.gobet@gmail.com>
2024-09-24 19:18:34 +0200

    Do some clean

--- src/TegCorpSharedKernelBundle.php
+++ src/TegCorpSharedKernelBundle.php
@@ -4,18 +4,8 @@ declare(strict_types=1);
 
 namespace TegCorp\SharedKernelBundle;
 
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class TegCorpSharedKernelBundle extends Bundle
 {
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
-    {
-        $loader = new YamlFileLoader($builder, new FileLocator(__DIR__.'/../Resources/config'));
-
-        $loader->load('messenger.yaml');
-    }
 }
