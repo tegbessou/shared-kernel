9d02be0e83025ebdcb3eeb94a9bcd4ad72ea374b
Tegbessou <hugues.gobet@gmail.com>
2024-09-24 09:03:07 +0200

    Do some clean

--- src/TegCorpSharedKernelBundle.php
+++ src/TegCorpSharedKernelBundle.php
@@ -4,23 +4,8 @@ declare(strict_types=1);
 
 namespace TegCorp\SharedKernelBundle;
 
-use Symfony\Component\DependencyInjection\ChildDefinition;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use TegCorp\SharedKernelBundle\Application\Command\AsCommandHandler;
-use TegCorp\SharedKernelBundle\Application\Query\AsQueryHandler;
 
 class TegCorpSharedKernelBundle extends Bundle
 {
-    #[\Override]
-    public function build(ContainerBuilder $container): void
-    {
-        $container->registerAttributeForAutoconfiguration(AsQueryHandler::class, static function (ChildDefinition $definition): void {
-            $definition->addTag('messenger.message_handler', ['bus' => 'query.bus']);
-        });
-
-        $container->registerAttributeForAutoconfiguration(AsCommandHandler::class, static function (ChildDefinition $definition): void {
-            $definition->addTag('messenger.message_handler', ['bus' => 'command.bus']);
-        });
-    }
 }
