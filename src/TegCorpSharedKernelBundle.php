43fd99ade9eeec7940c93ddbc505bd685f40bf4a
Tegbessou <hugues.gobet@gmail.com>
2024-09-25 21:03:16 +0200

    Do some clean

--- src/TegCorpSharedKernelBundle.php
+++ src/TegCorpSharedKernelBundle.php
@@ -5,14 +5,28 @@ declare(strict_types=1);
 namespace TegCorp\SharedKernelBundle;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\Yaml\Yaml;
+use TegCorp\SharedKernelBundle\Application\Command\AsCommandHandler;
+use TegCorp\SharedKernelBundle\Application\Query\AsQueryHandler;
 
 class TegCorpSharedKernelBundle extends AbstractBundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->registerAttributeForAutoconfiguration(AsQueryHandler::class, static function (ChildDefinition $definition): void {
+            $definition->addTag('messenger.message_handler', ['bus' => 'query.bus']);
+        });
+
+        $container->registerAttributeForAutoconfiguration(AsCommandHandler::class, static function (ChildDefinition $definition): void {
+            $definition->addTag('messenger.message_handler', ['bus' => 'command.bus']);
+        });
+    }
+
     public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $config = Yaml::parseFile(__DIR__.'/../config/messenger.yaml');
